7178f5c7a5731847c7a54b460755af493d1c92dc
Enrico Marquez <124639774+Enric0Mz@users.noreply.github.com>
2025-01-15 15:30:02 +0000

    build: change directory config

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,8 @@
+import "./Banner.css"
+export default function Banner() {
+    return (
+        <header className="banner">
+            <img src="/images/banner-brownie.jpg" alt="Banner com imagens de brownie"/>
+        </header>
+    )
+}
